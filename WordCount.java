c24d23bf146fe3369ec6daecbc63fee2e06ed056
EunbInn <dsino@naver.com>
2021-03-30 01:33:17 +0900

    WordCount - add comments

---
+++ WordCount.java
@@ -0,0 +1,52 @@
+package doitnow;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class WordCount {
+	public static void main(String[] args) { 	
+		WordCount main = new WordCount();
+		
+		main.getWordCount();
+		
+	}
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public void getWordCount() {
+		String input = sc.nextLine();
+		String word = sc.next();
+		String[] inputArr = input.split("");
+		String[] wordArr = word.split("");
+		int[] alphcnt = new int[wordArr.length];
+		
+		for (int i = 0; i < wordArr.length; i++) {
+			int cnt = 0;
+			int cnt2 = 0;
+			for (int j = 0; j < inputArr.length; j++) {
+				if (wordArr[i].equals(inputArr[j])) {
+					cnt++;
+				}
+			}
+			for (int k = 0; k < wordArr.length; k++) {
+				if (wordArr[i].equals(wordArr[k])) {
+					cnt2++;
+				}
+			}
+			alphcnt[i] = cnt / cnt2;
+		}
+
+		int min = alphcnt[0];
+		for (int i = 0; i < wordArr.length; i++) {
+			if (alphcnt[i] <= min) {
+				min = alphcnt[i];
+			} else {
+				
+			}
+		}
+		System.out.println(min);
+	}
+}
+
+
+
